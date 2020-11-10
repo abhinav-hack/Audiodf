60e0c26766515a7e1dc2973583b5597473e4e27f
abhihack <abhijeetbhavankar309@gmail.com>
2020-11-10 12:02:56 +0530

    added Combined moel file to git to track:

---
+++ Combined_model.py
@@ -0,0 +1,35 @@
+from numba import jit, cuda
+import tensorflow as tf
+from tensorflow import keras
+from keras.layers import Dense, Input, Activation
+from keras.layers import BatchNormalization, concatenate
+from keras.models import Model, Sequential, load_model
+from keras.optimizers import Adam
+from keras.losses import mse
+import soundfile as sf
+from Encoder_model import build_encoder
+from Synthesizer_model import build_synthesizer
+from Vocoder_model import build_vocoder
+from Synthesizer_preprocess import *
+from Encoder_preprocess import *
+
+def build_combined_model():
+    encoder = build_encoder()
+    synthesizer = build_synthesizer()
+    vocoder = build_vocoder()
+
+    voice_map_input = Input(shape=(N_MFCC*3, 345, 1))
+    signal_input = Input(shape=(345, N_MELS))
+    comb_enc = encoder(voice_map_input)
+    comb_syn = synthesizer(signal_input)
+    comb_lyr = concatenate([comb_enc, comb_syn])
+    output_model = vocoder(comb_lyr)
+
+    combined = Model(inputs=[signal_input, voice_map_input], outputs=output_model, name='combined')
+    combined.summary()
+
+    keras.utils.plot_model(combined, show_shapes=True)
+    return combined
+
+if __name__ == "__main__":
+    combined = build_combined_model()
